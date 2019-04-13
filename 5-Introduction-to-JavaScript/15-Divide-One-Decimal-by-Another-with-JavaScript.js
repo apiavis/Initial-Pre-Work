ae3218b25cd17b782706f0f966af0f7d3b29cf0f
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:54:13 -1000

    Add code for 15th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/15-Divide-One-Decimal-by-Another-with-JavaScript.js
@@ -0,0 +1,8 @@
+/*Author: Alicia Piavis
+  Title: Divide One Decimal by Another with JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to divide one decimal by another in JS using the
+     / operator between two decimal numbers.*/
+
+var quotient = 4.4 / 2.0;
