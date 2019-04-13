c29a997a5b290d1642bf41b2eaeae04b1a5ba41e
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 10:02:49 -1000

    Add code for 41st lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,17 @@
+/*Author: Alicia Piavis
+  Title: Manipulate Arrays With push()
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to append an element onto the end of an array using
+    the push() function.*/
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
