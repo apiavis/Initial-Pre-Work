852a96c3eaea9c079791be4f92d8ff12893a9597
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 08:06:08 -1000

    Add code for 20th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,16 @@
+/*Author: Alicia Piavis
+  Title: Compound Assignment with Augmented Division
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to do a mathematical operation and a variable
+     assignment in one step using the /= operator.*/
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
