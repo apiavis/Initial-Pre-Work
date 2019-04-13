622301db140ae6dc7e3e5ca8fb1e58386ca986b8
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:41:05 -1000

    Add code for tenth lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/10-Divide-One-Number-by-Another-with-JavaScript.js
@@ -0,0 +1,8 @@
+/*Author: Alicia Piavis
+  Title: Divide One Number by Another with JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to divide one number by another in JS using the / operator,
+     which allows for division when placed between two numbers.*/
+
+var quotient = 66 / 33;
