4b90805b883052e5cadae45686ed488a992c82e4
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:37:40 -1000

    Add code for eighth lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/8-Subtract-One-Number-from-Another-with-JavaScript.js
@@ -0,0 +1,8 @@
+/*Author: Alicia Piavis
+  Title: Subtract One Number from Another With JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to subtract one number from another in JS using
+    the - operator, which allows for subtraction when placed between two numbers.*/
+
+var difference = 45 - 33;
