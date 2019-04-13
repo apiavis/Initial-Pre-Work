a57e611a99bdd20eff812a429c749067f2993989
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:33:16 -1000

    Add code for sixth lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,17 @@
+/*Author: Alicia Piavis
+  Title: Understanding Case Sensitivty in Variables
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use proper JS naming conventions to name variables
+    using camelCase. In addition, variables and function names are case
+    sensitive.*/
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
