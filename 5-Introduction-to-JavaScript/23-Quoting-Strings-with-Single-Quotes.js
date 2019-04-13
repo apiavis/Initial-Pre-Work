5a863866c149f365caafe7c8b9ba75f5884bad45
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 08:18:49 -1000

    Add code for 23rd lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,9 @@
+/*Author: Alicia Piavis
+  Title: Quoting Strings with Single Quotes
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use a combination of single and double quotes to
+     use quotation marks or an apostrophe within a string, without having to use
+     escape characters or ending the string early.*/
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
