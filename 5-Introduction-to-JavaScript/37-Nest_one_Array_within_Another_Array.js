c763009669d31baece35b1861d0f58018b808d12
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 09:51:14 -1000

    Add code for 37th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,12 @@
+/*Author: Alicia Piavis
+  Title: Nest One Array within Another Array
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to nest one array inside of another using multiple sets
+    of brackets.*/
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [[2, 4], [3, 5]];
