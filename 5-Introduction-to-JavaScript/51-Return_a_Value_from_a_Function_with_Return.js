b1e2a2a544994657f0c774d8eaa6bbf808c674bd
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 10:57:10 -1000

    Add code for 51st lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,19 @@
+/*Author: Alicia Piavis
+  Title: Return a Value from a Function with Return
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to send a value back out of a function using a return
+    statement.*/
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(2));
+console.log(minusSeven(10));
