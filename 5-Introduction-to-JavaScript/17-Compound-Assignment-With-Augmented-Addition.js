e6eb816f81203842784409c3ba9113ba9cef3779
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 08:01:52 -1000

    Add code for 17th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,16 @@
+/*Author: Alicia Piavis
+  Title: Compound Assignment with Augmented Addition
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to do a mathematical operation and a variable
+     assignment in one step using the += operator.*/
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
