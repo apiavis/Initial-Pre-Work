5eddf1634f6003141d125d0ddaa18aa03b3e4120
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 08:31:56 -1000

    Add code for 24rd lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,9 @@
+/*Author: Alicia Piavis
+  Title: Escape Sequences in Strings
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use multpile escape characters within a single string
+     to use characters that could not otherwise be typed out, and to change
+     the spacing of a string when it is printed to the screen.*/
+
+var myStr = '"FirstLine\n\t\\SecondLine\nThirdLine"'; // Change this line
