ca8f90af068feec9dca081bdcc7f046b577d9272
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 09:15:00 -1000

    Add code for 27th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,14 @@
+/*Author: Alicia Piavis
+  Title: Constructing Strings with Variables
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to insert variables into a string that is being built
+     by using the + operator along with the name of the variable(s).*/
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Alicia Piavis";
+var myStr = "My name is " + myName + " and I am well!";
