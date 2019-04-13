94ecfc78a5e358ac27e9a149b4aa716191ce5eec
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:45:06 -1000

    Add code for 11th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/11-Increment-a-Number-with-JavaScript.js
@@ -0,0 +1,11 @@
+/*Author: Alicia Piavis
+  Title: Increment a Number with JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to increment a number using ++. This eliminates the need
+     for the = operator.*/
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
