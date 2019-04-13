06b140a57c1146d378b07971012be931206ffad7
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 08:03:12 -1000

    Add code for 18th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,16 @@
+/*Author: Alicia Piavis
+  Title: Compound Assignment with Augmented Subtraction
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to do a mathematical operation and a variable
+     assignment in one step using the -= operator.*/
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
