ce8957e5ae2c56e6b717a580d4545ddf14d1331e
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 08:04:36 -1000

    Add code for 19th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,16 @@
+/*Author: Alicia Piavis
+  Title: Compound Assignment with Augmented Multiplication
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to do a mathematical operation and a variable
+     assignment in one step using the *= operator.*/
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
