0baa25754548257ecf999c95d9f512043eb93a7a
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:35:35 -1000

    Add code for seventh lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/7-Add-Two-Numbers-with-JavaScript.js
@@ -0,0 +1,8 @@
+/*Author: Alicia Piavis
+  Title: Add Two Numbers With JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to add two numbers in JS using the + operator, which
+    allows for addition when placed between two numbers..*/
+
+var sum = 10 + 10;
