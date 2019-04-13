00feb3983ed081d062e815ac8f4b8730d088a7da
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 11:47:31 -1000

    Add code for 60th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,18 @@
+/*Author: Alicia Piavis
+  Title: Comparison with the Inequality Operator
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use the inequality operator != to determine whether
+    or not two values are  not equivalent.  If not equivalent, returns true.  If
+    equivalent, returns false.*/
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
