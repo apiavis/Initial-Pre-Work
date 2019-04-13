4aec7680564aa5bf82cd816821b9123fc6e2e04a
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 09:59:52 -1000

    Add code for 40th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,12 @@
+/*Author: Alicia Piavis
+  Title: Access Multi-Dimensional Arrays with Indexes
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to access an element within a multi-dimensional array
+    using bracket notation.*/
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
