a151afdff9bf153521ebfc9b14be3abedb0c469c
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 11:50:09 -1000

    Add code for 61st lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,23 @@
+/*Author: Alicia Piavis
+  Title: Comparison with the Strict Inequality Operator
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use the strict inequality operator !== to determine
+    whether or not two values are strictly inequivalent.  If the values seem
+    equivalent but are different data types, the conditional returns true.*/
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
