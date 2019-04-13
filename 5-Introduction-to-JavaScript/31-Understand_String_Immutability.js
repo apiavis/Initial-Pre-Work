b52a14e994244bd44cd3261d1fdac9857291d87e
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 09:28:28 -1000

    Add code for 31st lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,14 @@
+/*Author: Alicia Piavis
+  Title: Understand String Immutability
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to edit a string by reassigning the value of the
+     string variable. Once strings are created, they cannot be changed, unless
+     through this method.*/
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
