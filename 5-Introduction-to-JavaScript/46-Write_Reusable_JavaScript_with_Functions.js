34b76eefb72a91a4cb7a951dea8cc53aa6120410
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 10:18:49 -1000

    Add code for 46th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,20 @@
+/*Author: Alicia Piavis
+  Title: Write Reusable JS with Functions
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to write a function in JS so that the code can be
+    reused. To invoke the function, type functionName();.*/
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
