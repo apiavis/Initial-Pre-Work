54b4a62a436412fe070c271311f21b856bccb357
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:21:08 -1000

    Add code for third lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,14 @@
+/*Author: Alicia Piavis
+  Title: Storing Values with the Assignment Operator
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to store a value for a variable using the assignment
+    operator (=).*/
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
