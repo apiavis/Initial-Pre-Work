655a796a312dd5c843388a9c17a0028c7c23f959
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 09:07:04 -1000

    Add code for 25th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,13 @@
+/*Author: Alicia Piavis
+  Title: Concatenating Strings with the Plus Operator
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to concatenate two strings into one using the
+     concatenate (+) operator.*/
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
