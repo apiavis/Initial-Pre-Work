429cd55e8d92d99793dc97dbb7464e9a144e00f4
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 11:43:45 -1000

    Edit the strict equality operator symbol in the Summary

--- 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -2,7 +2,7 @@
   Title: Comparison with the Strict Equality Operator
   Date: 4/13/2019
   Project: Techtonic Pre-Work
-  Summary: I learned how to use the strict equality operator ==+ to determine
+  Summary: I learned how to use the strict equality operator === to determine
     whether or not two values are strictly equivalent.  If the values seem
     equivalent but are different data types, the conditional returns false.*/
 
