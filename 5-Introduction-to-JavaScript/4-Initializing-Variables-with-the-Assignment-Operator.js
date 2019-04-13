5ff45848b0cb96483affee1d30d47d191f8ee087
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:23:51 -1000

    Add code for fourth lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,12 @@
+/*Author: Alicia Piavis
+  Title: Initializing Variables with the Assignment Operator
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to initialize a variable to hold a specific value at
+    the same time that the variable is declared.*/
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
