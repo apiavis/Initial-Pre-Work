d9e0bba2419233587c467980f4f7c3973c5e77b3
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:39:33 -1000

    Add code for ninth lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/9-Multiply-Two-Numbers-with-JavaScript.js
@@ -0,0 +1,8 @@
+/*Author: Alicia Piavis
+  Title: Multiply Two Numbers with JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to multiply two numbers in JS using the * operator,
+     which allows for multiplication when placed between two numbers.*/
+
+var product = 8 * 10;
