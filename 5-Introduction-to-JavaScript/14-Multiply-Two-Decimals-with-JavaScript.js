c71b1c82f9e552199ec7f81693660cc2dc7dbb05
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:52:31 -1000

    Add code for 14th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/14-Multiply-Two-Decimals-with-JavaScript.js
@@ -0,0 +1,8 @@
+/*Author: Alicia Piavis
+  Title: Multiply Two Decimals with JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to multiply two decimal numbers in JS using the
+     * operator between two decimal numbers.*/
+
+var product = 2.0 * 2.5;
