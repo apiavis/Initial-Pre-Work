184d1695b5460db984c170c0e8b22f0c00c8a473
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 11:36:13 -1000

    Add code for 57th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,18 @@
+/*Author: Alicia Piavis
+  Title: Comparison with the Equality Operator
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use the equality operator == to determine whether
+    or not two values are equivalent.  If equivalent, returns true.  If not
+    equivalent, returns false.*/
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
