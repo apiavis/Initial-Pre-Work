aeb194ce7d3c4b8ebd97d96c25d32138814dfa5f
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:46:57 -1000

    Add code for 12th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,11 @@
+/*Author: Alicia Piavis
+  Title: Decrement a Number with JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to decrement a number using --. This eliminates the need
+     for the = operator.*/
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
