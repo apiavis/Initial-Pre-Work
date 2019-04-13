418b538aeda770eb5c44d8fb8eed3faaaf6a6df3
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 10:14:40 -1000

    Add code for 45th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,8 @@
+/*Author: Alicia Piavis
+  Title: Shopping List
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to create a multi-dimensional array by declaring
+    arrays within arrays.*/
+
+var myList = [["toothpaste", 1], ["oatmeal", 5], ["apples", 3], ["milk", 1], ["shampoo", 1]];
