26cd6796ce131758510695acae034fc01e3a855a
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 08:09:14 -1000

    Add code for 21st lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,14 @@
+/*Author: Alicia Piavis
+  Title: Declare String Variables
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to declare string variables in JS using quotations
+     around the text.*/
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Alicia";
+var myLastName = "Piavis";
