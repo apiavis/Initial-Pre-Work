6df2e95a82a117639b8f9dc0c6054ac988a0501c
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 11:17:54 -1000

    Add code for 55th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,15 @@
+/*Author: Alicia Piavis
+  Title: Understanding Boolean Values
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use a new data type. Booleans can only have two
+    values--true or false--and they are not written with quotes.*/
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
