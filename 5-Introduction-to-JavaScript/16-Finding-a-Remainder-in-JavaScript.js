16d0e13a00fead789880ae24c69371783f231dbf
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:57:51 -1000

    Add code for 16th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,8 @@
+/*Author: Alicia Piavis
+  Title: Finding a Remainder in JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to find the remainder of the division of two numbers
+     in JS using the remainder (%) operator.*/
+
+var remainder = 11 % 3;
