5c8aa7aad8bb5107fde8936b62b68bd5d79f4e55
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 08:12:30 -1000

    Add code for 22nd lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,9 @@
+/*Author: Alicia Piavis
+  Title: Escaping Literal Quotes in Strings
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use quotation marks within a string without
+     signifying the end of the string by using a backslash (\) before the
+     quotation mark.*/
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
