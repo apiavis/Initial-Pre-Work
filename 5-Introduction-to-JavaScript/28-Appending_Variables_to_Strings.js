311ef072a47c5dfde69207e1ae3505453adc2eed
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 09:18:31 -1000

    Add code for 28th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,17 @@
+/*Author: Alicia Piavis
+  Title: Appending Variables to Strings
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to append a variable to a string using the +=
+     operator.*/
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fun";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
