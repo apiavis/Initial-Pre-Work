d6d9761a0e3b0c98fabb853aa4fd46113612eb9d
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 09:56:15 -1000

    Add code for 39th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,16 @@
+/*Author: Alicia Piavis
+  Title: Modify Array Data with Indexes
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to modify array data using bracket notation and the
+    index of the array element that will be modified.*/
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
