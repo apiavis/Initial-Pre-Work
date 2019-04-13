6a514bc21cf123ec3807e6cc2f70d5a673365490
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:18:04 -1000

    Add code for second lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,11 @@
+/*Author: Alicia Piavis
+  Title: Declare JS Variables
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to declare and name variables in JS.*/
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
