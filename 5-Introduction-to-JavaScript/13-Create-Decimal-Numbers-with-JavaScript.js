3453a60017f875e2ee7570020f06d9c1f46428b2
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-13 07:50:32 -1000

    Add code for 13th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,12 @@
+/*Author: Alicia Piavis
+  Title: Create Decimal Numbers with JS
+  Date: 4/13/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to assign a decimal value to a variable. Decimals in
+     computer science are commonly called floats. However, not all real numbers
+     can be expressed as floats, because this can cause rounding errors.*/
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 5.7;
