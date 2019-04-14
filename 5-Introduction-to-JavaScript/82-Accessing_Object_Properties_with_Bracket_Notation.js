2586cf4a4d303f2395d4ffcc5616d073f3550d63
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 08:35:14 -1000

    Add code for 82nd lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,18 @@
+/*Author: Alicia Piavis
+  Title: Accessing Object Properties with Bracket Notation
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to access properties of objects in JS using bracket
+    notation.*/
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
