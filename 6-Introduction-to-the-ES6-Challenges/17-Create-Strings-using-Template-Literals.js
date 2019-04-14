39bdacb9102964f1c2284c9610c6a6e991a0e524
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 13:55:27 -1000

    Add code for 17th lesson of ES6 Challenges module and what was learned

---
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,28 @@
+/*Author: Alicia Piavis
+  Title: Create Strings Using Template Literals
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use template literals to create multi-line strings
+    and remove the need to use concatentation with the + operator.*/
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = arr.map(item => `<li class="text-warning">${item}</li>`);
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
