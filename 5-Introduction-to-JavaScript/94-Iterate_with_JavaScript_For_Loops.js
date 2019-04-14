29501beb109ddaf5c911c8dbbeebd3e34be5f99b
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 10:02:41 -1000

    Add code for 94th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,21 @@
+/*Author: Alicia Piavis
+  Title: Iterate with JS For Loops
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use a JS for loop to iterate through a section of
+    code a specified number of times.*/
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
