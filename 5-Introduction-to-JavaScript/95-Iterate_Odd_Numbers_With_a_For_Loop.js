8cbfaa60834b2b09b0f26a802ee5f4973e4004c8
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 10:05:16 -1000

    Add code for 95th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,21 @@
+/*Author: Alicia Piavis
+  Title: Iterate Odd Numbers with a For Loop
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use a JS for loop to iterate through a section of
+    code a specified number of times while counting by odd numbers.*/
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
