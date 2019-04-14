6028ca1e60babad7fb796d4d25094993633cdf54
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 08:41:28 -1000

    Add code for 83rd lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,17 @@
+/*Author: Alicia Piavis
+  Title: Accessing Object Properties with Variables
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to access properties of objects in JS using variables.*/
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
