41e77c111f5e6c9841b380bef86b4f27f8915a3b
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 08:26:01 -1000

    Add code for 80th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,23 @@
+/*Author: Alicia Piavis
+  Title: Build JS Objects
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to build an object in JS to store data in a structured
+    way. The object I built is called myDog and has 4 properties.*/
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Astronaut",
+  "legs":3,
+  "tails": 1,
+  "friends": ["Butch", "Princess", "Cheyenne"]
+};
