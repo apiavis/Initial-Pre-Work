ff74bfc29d9b745896d7fed8cd327512d9ede480
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 09:57:02 -1000

    Add code for 93rd lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,16 @@
+/*Author: Alicia Piavis
+  Title: Iterate with JS While Loops
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use a JS while loop to iterate through a section of
+    code until a specific condition is no longer true.*/
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
