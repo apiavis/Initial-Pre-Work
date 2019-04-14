b1d04abd06229aaf87b6d88fb7d898c4e80afb54
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 11:09:19 -1000

    Add code for 106th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,12 @@
+/*Author: Alicia Piavis
+  Title: Use the Conditional (Ternary) Operator
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use the conditional operator to simplifiy code when
+    you might otherwise use an if/else statement to check a simple condition.*/
+
+function checkEqual(a, b) {
+  return a === b ? true : false;
+}
+
+checkEqual(1, 2);
