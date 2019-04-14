6de4f7bd033f38bb870893abff508941e069ec42
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 10:08:56 -1000

    Add code for 96th lesson of JavaScript module and what was learned

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,21 @@
+/*Author: Alicia Piavis
+  Title: Count Backwards with a For Loop
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to count backwards using a JS for loop to decrement
+    from nine to 1 in intervals of two.*/
 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
