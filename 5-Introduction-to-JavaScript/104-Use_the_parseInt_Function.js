fd792a326ed612e2777f32aa6be85fbc5dbc6aab
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 10:58:44 -1000

    Add code for 104th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,12 @@
+/*Author: Alicia Piavis
+  Title: Use the parseInt Function
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use the parseInt() function in JS to parse a string
+    and return an integer.*/
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
