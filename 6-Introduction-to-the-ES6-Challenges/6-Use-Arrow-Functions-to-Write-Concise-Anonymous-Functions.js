c6b9b0a024a2966b096a1e81a99f55e6ed17b0f6
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 12:07:05 -1000

    Add code for sixth lesson of ES6 Challenges module and what was learned

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,11 @@
+/*Author: Alicia Piavis
+  Title: Use Arrow Functions to Write Concise Anonymous Functions
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to write concise functions using arrow functions when
+    writing inline functions that will not be reused anywhere else.*/
+
+const magic = () => {
+  "use strict";
+  return new Date();
+};
