e26f162d1630f17c35addea086e38b7c56cc794b
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 07:14:07 -1000

    Add code for 77th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,15 @@
+/*Author: Alicia Piavis
+  Title: Returning Boolean Values from Functions
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use use a shortcut and replace an if/else statement
+    that performs a comparison with a simple return statement inside a function,
+    since all comparison operators return a Boolean value.*/
+
+function isLess(a, b) {
+  // Fix this code
+    return a < b;
+}
+
+// Change these values to test
+isLess(7, 2);
