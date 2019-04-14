9135a2aeb04855b52d6d46688a5a27bb2d83233d
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 09:30:05 -1000

    Add code for 89th lesson of JavaScript module and what was learned

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,30 @@
+/*Author: Alicia Piavis
+  Title: Manipulating Complex Objects
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to create a complex data structure in JS by building an
+    array with two objects inside.*/
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Guster",
+    "title": "Look Alive",
+    "release_year": 2019,
+    "formats": [
+      "cd",
+      "mp3",
+      "mp4"
+    ]
+  }// Add record here
+]
