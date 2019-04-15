59b41876797d2f3d7e65fc1c67ff224a1e3988f8
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 14:45:05 -1000

    Add code for 23rd lesson of ES6 Challenges module and what was learned

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,10 @@
+/*Author: Alicia Piavis
+  Title: Use Export to Reuse a Code Block
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to export functions and variables so that they can
+    be reused in other files.*/
+
+"use strict";
+export const bar = "foo";
+export const foo = "bar";
