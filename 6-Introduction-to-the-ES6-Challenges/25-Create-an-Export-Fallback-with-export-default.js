c44044740faf5708a5e21f97c88e646d1c7542c9
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 14:52:49 -1000

    Add code for 25th lesson of ES6 Challenges module and what was learned

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,9 @@
+/*Author: Alicia Piavis
+  Title: Create an Export Fallback with export default
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use export default to create a fallback value
+    for a file or module. It cannot be used with var, let, or const.*/
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
