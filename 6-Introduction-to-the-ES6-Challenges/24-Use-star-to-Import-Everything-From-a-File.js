5104f0f24b4a331423a2a976c67ea1f6c99a2ad6
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 14:49:43 -1000

    Add code for 24th lesson of ES6 Challenges module and what was learned

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-star-to-Import-Everything-From-a-File.js
@@ -0,0 +1,9 @@
+/*Author: Alicia Piavis
+  Title: Use * to Import Everything from a File
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use import * to import everything from a file. It
+    does require that an object receives the imported values.*/
+
+"use strict";
+import * as stringFunctions from "capitalize_strings";
