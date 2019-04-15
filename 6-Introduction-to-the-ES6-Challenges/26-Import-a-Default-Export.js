a7b64c8554f253ee99474a8b1a14b9f6d67f77e1
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 14:56:10 -1000

    Add code for 26th lesson of ES6 Challenges module and what was learned

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,11 @@
+/*Author: Alicia Piavis
+  Title: Import a Default Export
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to import a default export by omitting the curly
+    braces around the imported value and just writing the value's name after
+    import.*/
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
