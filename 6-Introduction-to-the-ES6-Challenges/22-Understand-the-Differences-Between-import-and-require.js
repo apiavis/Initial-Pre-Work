add5bca6a2f2ce4504bb2db49b7b634d565ff138
apiavis <39506750+apiavis@users.noreply.github.com>
2019-04-14 14:40:01 -1000

    Add code for 22nd lesson of ES6 Challenges module and what was learned

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,11 @@
+/*Author: Alicia Piavis
+  Title: Understand the Differences Between import and require
+  Date: 4/14/2019
+  Project: Techtonic Pre-Work
+  Summary: I learned how to use an import statement to bring in just the desired
+    function from a file, rather than all functions, which would occur when
+    using a require statement.*/
+
+"use strict";
+import { capitalizeString } from "string_functions"
+capitalizeString("hello!");
